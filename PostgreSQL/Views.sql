3d2b9e06bdf73ee56baea46dd441612d516dcb47
fred8728 <fskn@hotmail.com>
2021-05-21 10:42:57 +0200

    customer crud done

--- PostgreSQL/Views.sql
+++ PostgreSQL/Views.sql
@@ -1,6 +1,13 @@
 
--- All orders
+-- Allorders
 CREATE OR REPLACE VIEW allOrders AS
-SELECT orders.order_id, customer."name", customer.telefon, orders.price,  orders.status 
+SELECT orders.order_id, customer."name", customer.phone, orders.price,  orders.status 
 FROM orders 
 JOIN customer ON (orders.customer_id = customer.customer_id);
+
+
+---------------
+-- AllCustomers
+CREATE OR REPLACE VIEW allCustomers AS 
+SELECT * FROM customer;
+
