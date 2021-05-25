346367b6d8b29b473e393f510540d983f1690c90
fred8728 <fskn@hotmail.com>
2021-05-25 15:36:07 +0200

    new project - deleted old project

--- Chocolate/src/main/java/Controller/HelloApplication.java
+++
@@ -1,9 +0,0 @@
-package Controller;
-
-import javax.ws.rs.ApplicationPath;
-import javax.ws.rs.core.Application;
-
-@ApplicationPath("/api")
-public class HelloApplication extends Application {
-
-}
