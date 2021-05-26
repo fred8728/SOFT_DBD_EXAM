8654af1f3b02ee37cc5395133da2961721ebed42
fred8728 <fskn@hotmail.com>
2021-05-26 15:05:34 +0200

    done for today with endpoints

--- Chocolate/src/main/java/com/example/spring/controller/OrderController.java
+++ Chocolate/src/main/java/com/example/spring/controller/OrderController.java
@@ -21,5 +21,35 @@ public class OrderController implements ErrorController {
         orderFacade = new OrderFacade();
     }
 
+    //Tested and works
+    // http://localhost:8080/allOrders
+    @GetMapping("allOrders")
+    public ArrayList<String> getOrders() throws SQLException {
+        return orderFacade.getOrders();
+    }
+
+    //Tested and works
+    // http://localhost:8080/orderDetails/someid
+    @GetMapping("orderDetails/{id}")
+    public ArrayList<OrderDetails> getOrderDetails(@PathVariable int id) throws SQLException {
+        return orderFacade.getOrderDetails(id);
+    }
+
+
+    // Needs to be tested
+    @DeleteMapping("deleteOrder/{id}")
+    public void deleteOrder(@PathVariable int id) throws SQLException {
+        orderFacade.deleteOrder(id);
+    }
+
+    // Needs to be tested
+    @PostMapping("order/add")
+    public void addOrder(Order order, ArrayList<OrderDetails> details){
+        try{
+            orderFacade.addOrder(order, details);
+        }catch (Exception ex){
+            ex.getMessage();
+        }
+    }
 
 }
